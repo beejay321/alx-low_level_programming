d6e9ac79d73d6ca03baeef7c6a7cacbecde61abb
Oluwabusola <jibodu.busola@gmail.com>
2023-03-16 12:54:16 -0700

    print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Print a line of code using puts
+ * printf should not be used
+ * program should return 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
