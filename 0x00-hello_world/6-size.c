8f37f6ebb4a1dc99dd4bbe2fb35b1c9ae1581c2e
Oluwabusola <jibodu.busola@gmail.com>
2023-03-16 16:38:20 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print out the size of the various types on the computer
+ * Return: 0
+*/
+int main(void)
+{
+	char _c;
+	int _i;
+	long int _l;
+	long long int _li;
+	float f;
+
+	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(_c));
+	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(_i));
+	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(_l));
+	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(_li));
+	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(_f));
+	return (0);
+}
