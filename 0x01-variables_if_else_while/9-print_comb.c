d8a7a21cd66002c31a26e3c55449bf7080f41e89
Oluwabusola <jibodu.busola@gmail.com>
2023-03-17 16:39:55 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio>
+/**
+ * main - program that prints all possible combinations of single-digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+if (num == '9')
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
