bb753f043b777449c1432463f8cb9af5c0c84a54
Oluwabusola <jibodu.busola@gmail.com>
2023-03-16 18:07:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Print a line of code without using printf and puts
  *
  * Return: 1
- */
+*/
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
