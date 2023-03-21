0a598a5dce1b429c44b5133a023f3a9adb927598
Oluwabusola <jibodu.busola@gmail.com>
2023-03-21 14:11:25 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++;)
+	_putchar(alph);
+	_putchar("\n")
+}
