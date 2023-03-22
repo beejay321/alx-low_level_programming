c497ba712ac23cb7e889d4437717f737b7379654
Oluwabusola <jibodu.busola@gmail.com>
2023-03-21 18:24:55 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Description - check code
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int i, j, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+		n = i * j
+		_putchar(n)
+		}
+		_putchar("\n")
+	}
+	return (n)
+}
