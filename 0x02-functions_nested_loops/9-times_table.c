ea12bdf23b37ea2862574946b1027355557b81ad
Oluwabusola <jibodu.busola@gmail.com>
2023-03-21 18:37:31 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,10 +13,28 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-		n = i * j;
-		_putchar(n);
+			n = i * j;
+			if ((n / 10) == 0)
+			{
+				if (j != 0)
+				_putchar(' ');
+				_putchar(n + '0');
+
+				if (j == 9)
+				continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+				continue;
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar("\n");
 	}
-	return (n);
 }
