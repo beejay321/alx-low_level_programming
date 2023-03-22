dfa25274bfe027ade87dff57f3fd2428af65c99c
Oluwabusola <jibodu.busola@gmail.com>
2023-03-21 18:43:58 -0700

    addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - check code
+ * @a: an integer
+ * @b: another integer
+ * Description: function that adds two integers
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
