b1b6400ce4740b5c70cfdf6fb79214055c6b3b5d
Oluwabusola <jibodu.busola@gmail.com>
2023-03-27 22:56:33 +0200

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Function that resets the value of a variable back to 98.
+ * @n:Variable to be reset.
+ * Return: Nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strlen - Function that returns the length of a string.
+ *@s: String pointer to the string whose length is to be found.
+ *Return: length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int p = 0;
+	
+	while (*(s + p) != 0)
+	{
+		p++;
+	}
+	return (p);
+}
+
